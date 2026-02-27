48ad1e3de95cd6e4970bdb1572d6827642fec93f
Reem Peleg <pwreem@gmail.com>
2026-02-27 16:04:40 +0200

    add composition methods to Base_Filter and add Hero Shot filter

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from engine import CameraEngine
 from Filters.composition_filters import *
+from Filters.advanced_filters import *
 from Filters.base_filter import Complexity
 
-
 if __name__ == "__main__":
+    engine = CameraEngine(model_path="model/yolov8n.pt")
 
-
-    engine = CameraEngine(model_path="model/yolov8n-pose.pt")
-
-    my_filter = CenteredFilter(
-        name="MainCenterFilter",
-        complexity=Complexity.LOW
+    my_filter = HeroShotFilter(
+        name="Hero Shot Pro",
+        complexity=Complexity.MEDIUM
     )
     other = RuleOfThirdsFilter(
         "RuleOfThirds",
         Complexity.LOW
     )
 
-    engine.set_filter(other)
+    engine.set_filter(my_filter)
 
     engine.run_live_camera()
